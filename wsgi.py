0fcada44ba891293259ad4e1450e1c102206a74b
JeremyVsquared <jeremy.vanv@gmail.com>
2015-08-02 18:46:26 -0400

    wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,9 @@
 import os
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "projops.settings")
 
 # This application object is used by the development server
 # as well as any WSGI server configured to use this file.
-from django.core.wsgi import get_wsgi_application
+
 application = get_wsgi_application()
